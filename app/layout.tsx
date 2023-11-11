bf2a2cfc7bd60e41ef8427a2e6d921dc59ab762d
moshiur <mansamoshiur@gmail.com>
2023-11-11 23:15:12 +0600

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "M-Shope",
+  description: "Modern Online Shope",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <div className="flex flex-col min-h-screen">
+          <main className="flex-grow">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
